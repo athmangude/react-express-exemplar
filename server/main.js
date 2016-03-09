d345ffa863193aefaab34a9fdc3d6bc8b8ae671f
Athman Gude <athmangude@gmail.com>
2016-03-09 08:16:16 +0300

    creating the initial scaffold

---
+++ server/main.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = new express();
+
+var port = 3001;
+
+app.get('/', function (request, response) {
+    response.render('./../app/index.ejs');
+});
+
+app.listen(port, function () {
+    console.log('listening on port', port);
+});
